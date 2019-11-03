f76cff4a1c7276b00bb0ff24d61ecebe14853d3b
rafaelfigueiredo-l <rafaael.figueiredo@gmail.com>
2019-11-03 01:30:24 -0200

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//https://thomastuts.com/blog/immutable-js-101-maps-lists.html
+//https://github.com/immutable-js/immutable-fromJS
+//https://www.freecodecamp.org/news/immutable-js-is-intimidating-heres-how-to-get-started-2db1770466d6/
+
+
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Map, List, fromJS } from 'immutable';
@@ -9,15 +14,19 @@ class App extends Component {
     super();
     this.state = {
       name: 'React',
-      data: Map({ count: 2, items: List() }),
-      data1: Map({ 
+      data: Map({ 
         a: 2, 
         itens_array: List([
-           'item1_list', 2, 3
+           1, 2, 3
         ]),
         itens_map: Map({
            item1: 'map1_value',
            item2: 'map2_value',
+        }),
+         itens_map2: Map({
+           name: 'Joao',
+           name: 'Maria',
+           name: 'Lucia',
         }),
         itens_array_object: Map({
            item1: 'map1_value',
@@ -35,13 +44,38 @@ class App extends Component {
 
 
   render() {
-    const {data, data1} = this.state;
-    const a2 = data1.getIn(['itens_array', 0]);
-    const a3 = data1.getIn(['itens_map', 'item1']);
+    const {data} = this.state;
+    const a0 = data.getIn(['itens_array', 0]);
+    const a1 = data.getIn(['itens_map', 'item1']);
+    const a2 = data.getIn(['book']);
+    const a3 = data.setIn(['itens_array', 5], 'teste');
+    const a4 = data.update(
+      'itens_array', itens_array => itens_array.push('a4 last')
+    );
+
     
-    console.log(data1);
+
+    
+    
+    console.log('%c Initial Data', 'color: green; font-weight: bold');
+    console.info(data);
+
+    console.log('%c a0 - get item array', 'color: green; font-weight: bold');
+    console.log(a0);
+
+    console.log('%c a1  - get item object', 'color: green; font-weight: bold');
+    console.log(a1);
+
+    console.log('%c a2 - get item function', 'color: green; font-weight: bold');
     console.log(a2);
+
+    console.log('%c a3 - update data with setIn function', 'color: blue; font-weight: bold');
     console.log(a3);
+
+    console.log('%c a4 - update data with push function', 'color: blue; font-weight: bold');
+    console.log(a4);
+
+    
 
     return (
       <div>
