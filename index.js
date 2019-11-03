0859bd795bfbb58802c391f56797f43763c66715
rafaelfigueiredo-l <rafaael.figueiredo@gmail.com>
2019-11-02 23:15:32 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Map, List } from 'immutable';
+import { Map, List, fromJS } from 'immutable';
 import Hello from './Hello';
 import './style.css';
 
@@ -10,22 +10,43 @@ class App extends Component {
     this.state = {
       name: 'React',
       data: Map({ count: 2, items: List() }),
-      data1: Map({ a: 2, items: List() }),
+      data1: Map({ 
+        a: 2, 
+        itens_array: List([
+           'item1_list', 2, 3
+        ]),
+        itens_map: Map({
+           item1: 'map1_value',
+           item2: 'map2_value',
+        }),
+        itens_array_object: Map({
+           item1: 'map1_value',
+           item2: 'map2_value',
+        }),
+        book: this.book()
+      })
     };
   }
+
+  book = () => {
+    return 'teste';
+  }
+
 
 
   render() {
     const {data, data1} = this.state;
+    const a2 = data1.getIn(['itens_array', 0]);
+    const a3 = data1.getIn(['itens_map', 'item1']);
+    
+    console.log(data1);
+    console.log(a2);
+    console.log(a3);
+
     return (
       <div>
         <Hello name={this.state.name} />
-        Count: {data.get('count')}
-        Count: {data1.get('a')}
-        
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        {this.book()}
       </div>
     );
   }
