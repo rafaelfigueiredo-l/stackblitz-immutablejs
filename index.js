68dd233870660e2023e53211a85660e122dbfd7a
rafaelfigueiredo-l <rafaael.figueiredo@gmail.com>
2019-11-03 02:25:59 -0200

    1 file modified

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ class App extends Component {
     this.state = {
       name: 'React',
       data: Map({ 
-        a: 2, 
         itens_array: List([
            1, 2, 3
         ]),
@@ -23,23 +22,21 @@ class App extends Component {
            item1: 'map1_value',
            item2: 'map2_value',
         }),
-         itens_map2: Map({
-           name: 'Joao',
-           name: 'Maria',
-           name: 'Lucia',
-        }),
-        itens_array_object: Map({
-           item1: 'map1_value',
-           item2: 'map2_value',
-        }),
-        book: this.book()
+        itens_array_object: List([
+           Map({name: 'Maria', sobrenome: 'Joana', age: 10}),
+           Map({name: 'Lucas', sobrenome: 'Amorim', age: 30}),
+           Map({name: 'Miguel', sobrenome: 'Torres', age: 40}),
+        ]),
+        candy: this.candy()
       })
     };
   }
 
-  book = () => {
-    return 'teste';
+  candy = () => {
+    return 'candy';
   }
+
+
 
 
 
@@ -47,13 +44,22 @@ class App extends Component {
     const {data} = this.state;
     const a0 = data.getIn(['itens_array', 0]);
     const a1 = data.getIn(['itens_map', 'item1']);
-    const a2 = data.getIn(['book']);
+    const a2 = data.getIn(['candy']);
     const a3 = data.setIn(['itens_array', 5], 'teste');
     const a4 = data.update(
       'itens_array', itens_array => itens_array.push('a4 last')
     );
+    const a5 = data.get('itens_array_object').findIndex(listing => {
+      return listing.get('name') === 'Miguel';
+    });
 
-    
+    const a6 = data.update(
+      'itens_array_object',
+      item => item.map(item => item.update(
+        'age',
+        age => age * 0.9 
+      ))
+    );
 
     
     
@@ -75,12 +81,18 @@ class App extends Component {
     console.log('%c a4 - update data with push function', 'color: blue; font-weight: bold');
     console.log(a4);
 
+    console.log('%c a5 - findIndex from some object', 'color: green; font-weight: bold');
+    console.log(a5);
+
+    console.log('%c a6 - update all ages from data', 'color: blue; font-weight: bold');
+    console.log(a6);
+
     
 
     return (
       <div>
         <Hello name={this.state.name} />
-        {this.book()}
+
       </div>
     );
   }
